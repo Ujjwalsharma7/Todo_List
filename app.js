3e64c1a39a442f8f46a85bf507aadfe1aad33c95
ujjwal <ujjwalsharma1772003@gmail.com>
2023-05-24 15:14:40 +0530

    added new route

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"));
 
 
-var items = ["Buy Food"," Eat food", "Cook food"];
+let items = ["Buy Food"," Eat food", "Cook food"];
+let workItems = []
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
@@ -24,17 +25,32 @@ app.get("/", function(req, res) {
   };
 var day = today.toLocaleDateString("en-US", options);
 
-res.render("list", {kindofDay: day, newListItems: items})
+res.render("list", {listTitle: day, newListItems: items})
 });
 
 app.post("/", function(req, res)  {
-  var item = req.body.newItem
 
- items.push(item);
+  let item = req.body.newItem
 
-  res.redirect("/");
+  if(req.body.list == "Work List"){
+    workItems.push(item)
+    res.redirect("/work")
+  }else{
+    items.push(item);
+    res.redirect("/")
+  }
+
 })
 
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems})
+})
+
+app.post("/work", function(req, res){
+  let item = req.body.newItem
+  workItems.push(item)
+  res.redirect("/ work")
+})
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
 });

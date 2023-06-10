1b7cbb973e0d8ae70750ae2b03969cfe204b1ead
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-10 17:50:48 +0530

    started database connection

--- app.js
+++ app.js
@@ -1,16 +1,55 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect('mongodb://127.0.0.1:27017/todolistDB')
+.then(() =>{
+  console.log("Mongo Connection Open")
+})
+.catch(err => {
+  console.log("Mongo not connected")
+  console.log(err)
+})
+
+
+
+const itemSchema = {
+  name: String
+}
+
+const Item = mongoose.model("Item", itemSchema)
+
+// const item1 = new Item({
+//   name: "Welcome to your todoList"
+// });
+
+// const item2 = new Item({
+//   name: "Hit the + button to add a new item"
+// })
+
+// const item3 = new Item({
+//   name: "<-- Hit this to delete an item"
+// })
+
+// const defaultItem = [item1, item2, item3]
+
+// Item.insertMany(defaultItem)
+// .then( res => {
+//   console.log("Succesfuly inserted items")
+// })
+// .catch(e => {
+//   console.log(e)
+// })
+
 
 app.get("/", function(req, res) {
 
@@ -44,3 +83,4 @@ app.get("/about", function(req, res){
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+

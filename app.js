adb053d218870e7e9aca95da5619c5187f724d42
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-10 16:46:35 +0530

    added date.js

--- app.js
+++ app.js
@@ -1,56 +1,46 @@
-//jshint esversion:6
-
 const express = require("express");
-
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}))
 
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
-let items = ["Buy Food"," Eat food", "Cook food"];
-let workItems = []
-app.set('view engine', 'ejs');
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.get("/", function(req, res) {
 
-  var today = new Date()
+const day = date.getDate();
 
-  var options ={
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-var day = today.toLocaleDateString("en-US", options);
+  res.render("list", {listTitle: day, newListItems: items});
 
-res.render("list", {listTitle: day, newListItems: items})
 });
 
-app.post("/", function(req, res)  {
+app.post("/", function(req, res){
 
-  let item = req.body.newItem
+  const item = req.body.newItem;
 
-  if(req.body.list == "Work List"){
-    workItems.push(item)
-    res.redirect("/work")
-  }else{
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
     items.push(item);
-    res.redirect("/")
+    res.redirect("/");
   }
+});
 
-})
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
 
-app.get("/work", function(req, res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems})
-})
+app.get("/about", function(req, res){
+  res.render("about");
+});
 
-app.post("/work", function(req, res){
-  let item = req.body.newItem
-  workItems.push(item)
-  res.redirect("/ work")
-})
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
